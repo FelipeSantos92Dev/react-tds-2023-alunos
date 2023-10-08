70537b4914f6547601e915581175d1414fdfe4c1
Felipe Santos <dev.felipesantos@gmail.com>
2023-10-08 12:27:52 -0300

    feat: use dashlist component

--- app/components/dashlist/DashList.jsx
+++ app/components/dashlist/DashList.jsx
@@ -3,11 +3,12 @@ import styles from './dashlist.module.css'
 
 const DashList = ({ lista, exclude, edit, tipo, color }) => {
   const cor = tipo == "Receita" ? "#9fe0b1" : "#e09f9f"
+  const texto = tipo == "Receita" ? "Receitas" : "Despesas"
 
   return (
     <div className={styles.registros} style={{ backgroundColor: color }}>
-      <p className={styles.registrosreceitastitle}>Receitas Registradas</p>
-      <div className={styles.registrosreceitaslist}>
+      <p className={styles.registrostitle}>{texto} Registradas</p>
+      <div className={styles.registroslist}>
         {
           lista.map(transacao => 
             transacao.tipo == tipo && (
--- app/finances/page.jsx
+++ app/finances/page.jsx
@@ -1,20 +1,20 @@
-"use client"
-import { useState } from 'react'
-import styles from './finances.module.css'
+"use client";
+import { useState } from "react";
+import styles from "./finances.module.css";
 
-import ListaTransacao from '@/models/ListaTransacao'
+import ListaTransacao from "@/models/ListaTransacao";
 
-import DashCard from '../components/dashcard/DashCard'
-import DashButton from '../components/dashbutton/DashButton'
-import DashItem from '../components/dashitem/DashItem'
-import DashList from '../components/dashlist/DashList'
+import DashCard from "../components/dashcard/DashCard";
+import DashButton from "../components/dashbutton/DashButton";
+import DashItem from "../components/dashitem/DashItem";
+import DashList from "../components/dashlist/DashList";
 
-const listaTransacao = new ListaTransacao()
+const listaTransacao = new ListaTransacao();
 
 function Finances() {
   // Inputs
-  const [description, setDescription] = useState('')
-  const [value, setValue] = useState('')
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
 
   // Dados da Classe
   const [lista, setLista] = useState(listaTransacao.getHistorico());
@@ -23,40 +23,40 @@ function Finances() {
   const [despesas, setDespesas] = useState(listaTransacao.despesas);
 
   // Edição
-  const [flag, setFlag] = useState(0)
-  const [editButton, setEditButton] = useState(false)
+  const [flag, setFlag] = useState(0);
+  const [editButton, setEditButton] = useState(false);
 
   // Adicionar Receita
   const addReceita = () => {
     // console.log("Adicionou receita");
-    listaTransacao.adicionarTransacao(description, value, "Receita")
-    
+    listaTransacao.adicionarTransacao(description, value, "Receita");
+
     atualizarValores();
-  }
+  };
 
   // Adicionar Despesa
   const addDespesa = () => {
     // console.log("Adicionou despesa");
-    listaTransacao.adicionarTransacao(description, value, "Despesa")
-    
+    listaTransacao.adicionarTransacao(description, value, "Despesa");
+
     atualizarValores();
-  }
+  };
 
   const exclude = (id) => {
-    listaTransacao.excluirTransacao(id)
+    listaTransacao.excluirTransacao(id);
 
     atualizarValores();
-  }
+  };
 
   const edit = (id) => {
     const transacao = listaTransacao.getTransacaoPorId(id);
-    
-    setDescription(transacao.descricao)
-    setValue(transacao.valor)
 
-    setEditButton(true)
-    setFlag(id)
-  }
+    setDescription(transacao.descricao);
+    setValue(transacao.valor);
+
+    setEditButton(true);
+    setFlag(id);
+  };
 
   const update = () => {
     listaTransacao.atualizarTransacao(flag, description, value);
@@ -64,17 +64,17 @@ function Finances() {
     atualizarValores();
     setEditButton(false);
     setFlag(0);
-  }
+  };
 
   const atualizarValores = () => {
-    setDescription('');
-    setValue('');
+    setDescription("");
+    setValue("");
 
     setSaldo(listaTransacao.saldo);
     setReceitas(listaTransacao.receitas);
     setDespesas(listaTransacao.despesas);
     setLista(listaTransacao.getHistorico());
-  }
+  };
 
   return (
     <>
@@ -94,50 +94,64 @@ function Finances() {
                 className={styles.inputdescription}
                 value={description}
                 type="text"
-                name='description'
-                placeholder='Descrição'
+                name="description"
+                placeholder="Descrição"
                 onChange={(e) => setDescription(e.target.value)}
               />
-              <input className={styles.inputdescription} value={value} type="number" name='description' placeholder='Valor (R$)' onChange={(e) => setValue(e.target.value)}/>
+              <input
+                className={styles.inputdescription}
+                value={value}
+                type="number"
+                name="description"
+                placeholder="Valor (R$)"
+                onChange={(e) => setValue(e.target.value)}
+              />
             </div>
             <div className={styles.type}>
-              {
-                editButton ? (
-                  <DashButton text={"Editar"} fn={update} color={"#9fc7e0"} />
-                ) : (
-                  <>
-                    <DashButton text={"Receita"} fn={addReceita} color={"#9fe0b1"} />
-                    <DashButton text={"Despesa"} fn={addDespesa} color={"#e09f9f"} />
-                  </>
-                )
-              }
+              {editButton ? (
+                <DashButton text={"Editar"} fn={update} color={"#9fc7e0"} />
+              ) : (
+                <>
+                  <DashButton
+                    text={"Receita"}
+                    fn={addReceita}
+                    color={"#9fe0b1"}
+                  />
+                  <DashButton
+                    text={"Despesa"}
+                    fn={addDespesa}
+                    color={"#e09f9f"}
+                  />
+                </>
+              )}
             </div>
           </div>
         </div>
         <div className={styles.infos}>
-          <DashCard titulo={'Saldo'} valor={saldo} cor={"#9fc7e0"} />
-          <DashCard titulo={'Receitas'} valor={receitas} cor={"#9fe0b1"} />
-          <DashCard titulo={'Despesas'} valor={despesas} cor={"#e09f9f"} />
+          <DashCard titulo={"Saldo"} valor={saldo} cor={"#9fc7e0"} />
+          <DashCard titulo={"Receitas"} valor={receitas} cor={"#9fe0b1"} />
+          <DashCard titulo={"Despesas"} valor={despesas} cor={"#e09f9f"} />
         </div>
 
         <div className={styles.registros}>
-          <DashList lista={lista} exclude={exclude} edit={edit} tipo={"Receita"} color={"#d0f0c0"} />
-
-          <div className={styles.registrosdespesas}>
-            <p className={styles.registrosdespesastitle}>Despesas Registradas</p>
-            <div className={styles.registrosdespesaslist}>
-            {
-                lista.map(transacao => 
-                  transacao.tipo == "Despesa" && (
-                    <DashItem transacao={transacao} exclude={exclude} edit={edit} cor={"#e09f9f"} />
-                  ))
-              }
-            </div>
-          </div>
+          <DashList
+            lista={lista}
+            exclude={exclude}
+            edit={edit}
+            tipo={"Receita"}
+            color={"#d0f0c0"}
+          />
+          <DashList
+            lista={lista}
+            exclude={exclude}
+            edit={edit}
+            tipo={"Despesa"}
+            color={"#f0c0c0"}
+          />
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Finances
+export default Finances;
